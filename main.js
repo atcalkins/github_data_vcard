6d17ba06ebbe993cd248b1168483cfb410f3e2f4
Tyler Calkins <atcalkins@outlook.com>
2017-07-18 10:45:56 -0400

    adjusted to fetch and fixed header padding and bold list

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 let wrapper = document.querySelector(".card");
 
-let request = new XMLHttpRequest();
-request.addEventListener("load", displayInfo);
-request.open("GET", "https://api.github.com/users/atcalkins");
-request.send();
+// XML Request code below ******
+// let request = new XMLHttpRequest();
+// request.addEventListener("load", displayInfo);
+// request.open("GET", "https://api.github.com/users/atcalkins");
+// request.send();
 
-function displayInfo() {
-  let formData = JSON.parse(this.responseText);
+fetch("https://api.github.com/users/atcalkins").then(function(response) {
+  if (response.status !== 200) {
+    console.log(response.status);
+    return;
+  }
+  response.json().then(function(data) {
+    console.log(data.name, response.url);
+    displayInfo(data);
+  });
+});
+
+function displayInfo(data) {
+  // let formData = JSON.parse(this.responseText);
+  // **** XML Request above *****
   let template = `
     <div class=headbox>
-    <h1>${formData.name}</h1>
+    <h1>${data.name}</h1>
     </div>
       <article class = undertitle>
       <div class=info>
         <h2>The Basics</h2>
         <ul>
-          <li><span class=text>Name:</span> ${formData.name}</li>
-          <li class=link><span class=text>GitHub URL:</span><a href= "${formData.html_url}"> atcalkins</a></li>
-          <li><span class=text>Company: </span> ${formData.company}</li>
-          <li class =link><span class=text>Website: </span><a href= "${formData.blog}"> Coolwebsite.com</a></li>
+          <li><span class=text>Name:</span> ${data.name}</li>
+          <li class=link><span class=text>GitHub URL:</span><a href= "${data.html_url}"> atcalkins</a></li>
+          <li><span class=text>Company: </span> ${data.company}</li>
+          <li class =link><span class=text>Website: </span><a href= "${data.blog}"> Coolwebsite.com</a></li>
         </ul>
       </div>
       <hr>
       <div class=bio>
         <h2>The Story</h2>
-          <p>${formData.bio}</p>
+          <p>${data.bio}</p>
       </div>
       <div class=picture>
-          <img src="${formData.avatar_url}">
+          <img src="${data.avatar_url}">
       </div>
       </article>
     `;
